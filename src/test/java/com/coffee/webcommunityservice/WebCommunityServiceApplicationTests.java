c240fe30aec21bcc8af486cbcb1ca570bb173ee2
kom-1010 <kom-1010@naver.com>
2021-10-23 14:36:40 +0900

    init springboot

---
+++ src/test/java/com/coffee/webcommunityservice/WebCommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.webcommunityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
